ddaed2218f9fa8cb5eaf62293cb984ebfc5972bc
Areum Jo <jar1214@naver.com>
2022-08-10 22:28:13 +0900

    220810 Java로 배우는 자료구조 알고리즘 Map, DFS, BFS

---
+++ src/main/java/com/example/practice/Algorithm/Map_exercise_1.java
@@ -0,0 +1,22 @@
+package com.example.practice.Algorithm;
+
+public class Map_exercise_1 {
+	
+	public int solution(int[] nums) {
+		//포켓몬 종류의 개수를 구함.
+		
+		Map<Integer, Integer> map = new HashMap<>();
+		for(int n : nums) {
+			map.put(n, 0);
+		}
+		//중복된 건 제외하고 넣어짐 
+		//아... 중복된 건 위에 덮어짐 
+		int n1 = map.keySet().size();
+		//배열 크기의 1/2을 구한다.
+		int n2 = nums.length/2;
+		
+		//최소값 선택 
+        return Math.min(n1, n2);
+    }
+
+}
